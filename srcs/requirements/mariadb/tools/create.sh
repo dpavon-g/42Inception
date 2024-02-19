1ac13b752955aad8a60bdfaefabf1269f8bc56b9
dpavon-g <pavongomezdaniel@gmail.com>
2024-02-19 12:22:07 +0100

    mariadb server working nginx in proccess

--- srcs/requirements/mariadb/tools/create.sh
+++ srcs/requirements/mariadb/tools/create.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-sleep 5
 
 service mariadb start 
 
-echo "CREATE DATABASE IF NOT EXISTS dpavon_db;" > create.sql
+sleep 5
+
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > create.sql
 echo "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'rootpass' ;" >> create.sql
-echo "GRANT ALL PRIVILEGES ON dpavon_db.* TO root@'%';" >> create.sql
-echo "CREATE USER IF NOT EXISTS 'dpavon-g'@'%' IDENTIFIED BY '12345' ;" >> create.sql
-echo "GRANT SELECT, INSERT ON dpavon_db.* TO 'dpavon-g'@'%';" >> create.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO root@'%';" >> create.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> create.sql
+echo "GRANT SELECT, INSERT ON $DB_NAME.* TO '$DB_USER'@'%';" >> create.sql
 echo "FLUSH PRIVILEGES;" >> create.sql
 
 mysql < create.sql
 
+# sleep 5
+
 service mariadb stop
 
-sleep 10
-
-mysqld
+mysqld_safe
